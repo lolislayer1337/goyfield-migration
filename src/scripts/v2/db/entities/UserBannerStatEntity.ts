7c770d547adc735be568798817cf54d24dbff2e4
lolislayer1337 <91721976+lolislayer1337@users.noreply.github.com>
2026-08-08 14:10:08 +0500

    UserBannerStatEntity

--- src/scripts/v2/db/entities/UserBannerStatEntity.ts
+++ src/scripts/v2/db/entities/UserBannerStatEntity.ts
@@ -1,5 +1,13 @@
 export interface UserBannerStatEntity {
     uid: string;
     bannerId: string;
-
+    totalPulls: number;
+    total6: number;
+    sumPity6: number;
+    total5: number;
+    sumPity5: number;
+    won5050: number;
+    total5050: number;
+    lastProcessedPullTime: bigint;
+    lastUpdate: Date;
 }
